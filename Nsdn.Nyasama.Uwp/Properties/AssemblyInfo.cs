1400adc16d13b23d68847693942670d679b267e6
tinyAdapter <tinyadapter@outlook.com>
2017-04-22 16:32:24 +0800

    添加磁贴和图标资源

--- Nsdn.Nyasama.Uwp/Properties/AssemblyInfo.cs
+++ Nsdn.Nyasama.Uwp/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nsdn.Nyasama.Uwp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("喵玉殿")]
+[assembly: AssemblyDescription("喵玉殿是一家东方Project中文论坛，是喵玉汉化组汉化作品发布站点。")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nyasama Developer Network")]
 [assembly: AssemblyProduct("Nsdn.Nyasama.Uwp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -24,6 +25,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("zh-CN")]
+
